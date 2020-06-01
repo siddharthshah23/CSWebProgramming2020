7d048c76f0ab695a7e47e4a1d731f6f9a49d1808
Siddharth Shah <info@siddharthshah.io>
2020-05-31 21:00:38 -0400

    CreditCardValidatorv2

--- Creditcrad.py
+++ Creditcrad.py
@@ -1,4 +1,4 @@
-ccnumber = input("Please enter the credit card number")
+ccnumber = input("Please enter the credit card number ")
 
 
 def validator(ccnumber):
@@ -16,12 +16,27 @@ def validator(ccnumber):
                 sum += multiplynumber % 10 + multiplynumber // 10
             else:
                 sum += multiplynumber
-        else :
+        else:
             sum += int(validnumber[j])
+
     if sum % 10 == 0:
-        print("Card is legit")
-    else:
-        print("Enter correct credit card number")
+        return bool
+        # print("Card is legit")
+    # else:
+    # print("Enter correct credit card number")
 
 
-validator(ccnumber)
+def cc(ccnumber):
+    abc = int(ccnumber[:2])
+    if abc // 10 == 4 and validator(ccnumber):
+        print("Visa")
+    if abc == 34 or abc ==37 and validator(ccnumber) :
+         print("American Express ")
+    if abc > 50 and abc <=55 and validator(ccnumber):
+        print("Master Card ")
+    else :
+        print("Please enter correct credit card number ")
+# validator(ccnumber)
+
+
+cc(ccnumber)
